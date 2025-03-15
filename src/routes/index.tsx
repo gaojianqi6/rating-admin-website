1082e27cd9b04947cf1855e29f3e0682801be76a
Jianqi Gao <jianqi.gao@carsome.com>
2025-03-15 16:36:00 +1300

    Add tanstack react router

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button"
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </div>
+  )
+}
