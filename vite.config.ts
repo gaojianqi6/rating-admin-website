611876fb8b8244e7c8fa63d7b41c19353d83e591
Jianqi Gao <jianqi.gao@carsome.com>
2025-05-21 20:31:50 +1200

    Add vercel plugins

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   plugins: [
+    vercel(),
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(), 
     tailwindcss(),
